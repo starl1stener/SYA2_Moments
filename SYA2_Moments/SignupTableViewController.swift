7ee1420f104b9c8c47c3b9417d136b1004c21cef
n0an <nantog@gmail.com>
2016-11-09 22:56:30 +0300

    sign up realized

--- SYA2_Moments/SignupTableViewController.swift
+++ SYA2_Moments/SignupTableViewController.swift
@@ -7,49 +7,134 @@
 //
 
 import UIKit
+import Firebase
 
 class SignupTableViewController: UITableViewController {
     
     @IBOutlet weak var profileImageView: UIImageView!
-    
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var fullNameTextField: UITextField!
-    
     @IBOutlet weak var userNameTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
     
-    
     var imagePickerHelper: ImagePickerHelper!
+    var profileImage: UIImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Create New Account"
         
-
-       
+        profileImageView.layer.cornerRadius = profileImageView.bounds.width / 2.0
+        profileImageView.layer.masksToBounds = true
+        
+        emailTextField.delegate = self
+        fullNameTextField.delegate = self
+        userNameTextField.delegate = self
+        passwordTextField.delegate = self
+        
     }
+    
+    
+    func showAlert(withMessage message: String) {
+        let errorAlert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        errorAlert.addAction(okAction)
+        self.present(errorAlert, animated: true, completion: nil)
 
-    @IBAction func actionCreateNewAccountButtonTapped(_ sender: Any) {
-        
+    }
+    
+
+    @IBAction func actionCreateNewAccountButtonTapped() {
         // create a new account
         // save the user data, take photo
         // login the user
-    }
-    
-    @IBAction func actionBackTapped(_ sender: Any) {
+        
+        if emailTextField.text != ""
+            && (passwordTextField.text?.characters.count)! > 6
+            && (userNameTextField.text?.characters.count)! > 6
+            && fullNameTextField.text != ""
+            && profileImageView != nil {
+            
+            let username = userNameTextField.text!
+            let fullName = fullNameTextField.text!
+            let email = emailTextField.text!
+            let password = passwordTextField.text!
+            
+            FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (firUser, error) in
+                if error != nil {
+                    // report error
+                    self.showAlert(withMessage: "\(error?.localizedDescription)")
+                    
+                } else if let firUser = firUser {
+                    
+                    let newUser = User(uid: firUser.uid, username: username, fullName: fullName, bio: "", website: "", follows: [], followedBy: [], profileImage: self.profileImage)
+                    newUser.save(completion: { (error) in
+                        
+                        if error != nil {
+                            // report error
+                            self.showAlert(withMessage: "\(error?.localizedDescription)")
+                            
+                        } else {
+                            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (firUser, error) in
+                                if let error = error {
+                                    // report error
+                                    self.showAlert(withMessage: "\(error.localizedDescription)")
+                                } else {
+                                    self.dismiss(animated: true, completion: nil)
+                                }
+                            })
+                        }
+
+                    })
+                }
+            })
+            
+        }
         
     }
     
+    @IBAction func actionBackTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
-    @IBAction func actionChangeProfilePhotoDidTap(_ sender: Any) {
+    
+    @IBAction func actionChangeProfilePhotoDidTap() {
+        
+        print("actionChangeProfilePhotoDidTap")
+        
+        imagePickerHelper = ImagePickerHelper(viewController: self, completion: { (image) in
+            self.profileImageView.image = image
+            self.profileImage = image
+        })
         
     }
 }
 
 
-
+extension SignupTableViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == emailTextField {
+            fullNameTextField.becomeFirstResponder()
+        } else if textField == fullNameTextField {
+            userNameTextField.becomeFirstResponder()
+        } else if textField == userNameTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            passwordTextField.resignFirstResponder()
+            
+            
+            actionCreateNewAccountButtonTapped()
+        }
+        
+        return true
+        
+    }
+    
+    
+}
 
 
 
